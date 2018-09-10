508c072ae22d9914a633b1a38dbdab043dbd8c91
Dubravko <woose@web.de>
2018-09-10 17:10:36 +0200

    annotations added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,8 +1,17 @@
 <?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Um Annotationen ausführen zu können muss man [composer require annotations] und
+ * 
+ */
+
 
 class ArticleController {
+/**
+      * @Route("/lucky/number")
+      */
 
     public function index(){
         return new Response("<html><body><h1>Hello</h1></body></html>");
